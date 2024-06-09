4ca1f1f2519b6abada141ba43c267d94cf583f2b
VirU <viru..@VirUs-MacBook-Air.local>
2024-06-09 19:21:32 +0530

    personal-shelf

--- src/Components/Book-search.js
+++ src/Components/Book-search.js
@@ -1,38 +1,35 @@
 import React, { useEffect, useState } from 'react';
-
 import { Link } from 'react-router-dom';
 import ClockLoader from "react-spinners/ClockLoader";
-
 
 const BookSearch = () => {
   const [input, setInput] = useState('');
   const [bookData, setBookData] = useState([]);
   const [error, setError] = useState('');
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false);
+
   const fetchData = async (inp) => {
-    
     try {
       if (inp?.length > 2) {
-        setLoading(true)
+        setLoading(true);
         const response = await fetch(`https://openlibrary.org/search.json?q=${inp}&limit=10&page=1`);
-        
         const res = await response.json();
         console.log(res);
         if (res.docs.length <= 0) {
-            setLoading(false)
+          setLoading(false);
           throw new Error('Book not found');
         }
         setBookData(res.docs);
-        setLoading(false)
+        setLoading(false);
         setError('');
       } else {
-          setBookData([]);
-        setLoading(false)
+        setBookData([]);
+        setLoading(false);
         setError('');
       }
     } catch (err) {
       setBookData([]);
-      setLoading(false)
+      setLoading(false);
       setError(err.message);
     }
   };
@@ -47,7 +44,7 @@ const BookSearch = () => {
   }, [input]);
 
   const handleInputChange = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     setInput(e.target.value);
   };
 
@@ -57,28 +54,29 @@ const BookSearch = () => {
     localStorage.setItem('bookshelf', JSON.stringify(bookshelf));
   };
 
+  const isBookInBookshelf = (book) => {
+    const bookshelf = JSON.parse(localStorage.getItem('bookshelf')) || [];
+    return bookshelf.some((storedBook) => storedBook.key === book.key);
+  };
+
   return (
     <div className="container mx-auto p-4">
-      
-     
-          <input
-            type="text"
-            value={input}
-            autoFocus
-            onChange={handleInputChange}
-            placeholder="Search Book here.."
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-       <Link to="/bookshelf" className="mt-4 inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+      <input
+        type="text"
+        value={input}
+        autoFocus
+        onChange={handleInputChange}
+        placeholder="Search Book here.."
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      />
+      <Link to="/bookshelf" className="mt-4 inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
         Go to My Bookshelf
       </Link>
       <div className='flex justify-center mt-3'>
-
-        {loading?<ClockLoader  color="#36d7b7" />:""}
+        {loading ? <ClockLoader color="#36d7b7" /> : ""}
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {error && <p className="text-red-500 text-sm">{error}</p>}
-        
         {bookData && bookData.map((book) => (
           <div key={book.key} className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white">
             <div className="px-6 py-4">
@@ -89,17 +87,18 @@ const BookSearch = () => {
               <p className="text-gray-600 text-sm">
                 {book.first_publish_year ? `First published: ${book.first_publish_year}` : 'N/A'}
               </p>
-              <button
-                onClick={() => addToBookshelf(book)}
-                className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-2"
-              >
-                Add to Bookshelf
-              </button>
+              {!isBookInBookshelf(book) && (
+                <button
+                  onClick={() => addToBookshelf(book)}
+                  className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mt-2"
+                >
+                  Add to Bookshelf
+                </button>
+              )}
             </div>
           </div>
         ))}
       </div>
-      
     </div>
   );
 };
